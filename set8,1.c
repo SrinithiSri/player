c80edd0fcfc9b847db9755b614ac91ce008be8f5
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-02-28 19:59:51 +0530

    Create set8,1.c

---
+++ set8,1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,a[50];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i]<a[i+1])
+        {
+            printf("%d ",a[i+1]);
+        }
+        else
+        {
+            printf("%d ",a[i]);
+        }
+    }
+
+    return 0;
+}
