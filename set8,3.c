aaf03f9d118e433c30d5b3076b7ff04e53028888
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-03-01 20:07:27 +0530

    Create set8,3.c

---
+++ set8,3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,a[100],i,count=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]-a[i+1]==1||a[i]+a[i+1==-1])
+        {
+            count++;
+        }
+    }
+    if(count==n)
+    {
+      for(i=0;i<n;i++)
+      {
+          if(a[i]==k)
+          {
+              printf("%d",i+1);
+          }
+      }
+    }
+    else
+    {
+        printf("invalid");
+    }
+    return 0;
+}
