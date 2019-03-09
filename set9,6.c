9b72d5f3ff073cc9f7b33e59532e788f48709af1
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-03-09 19:18:26 +0530

    Create set9,6.c

---
+++ set9,6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,temp;
+    scanf("%d",&n);
+    if(n<=10000)
+    {
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        temp=a[0];
+        for(i=0;i<n-1;i++)
+        {
+            temp=(temp^a[i+1]);
+        }
+    }
+    printf("%d",temp);
+    return 0;
+}
