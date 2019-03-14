1354cea468ae256e05a089fc285fd161d540e6cf
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-03-14 20:32:36 +0530

    Create set10,8.c

---
+++ set10,8.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    long int k,n,c=0,s=0,rem,t,i;
+    scanf(" %ld %ld",&n,&k);
+    t=n;
+    while(t)
+    {
+        t=t/10;
+        s++;
+    }
+    while(n)
+    {
+        rem=n%10;
+        if(rem>=0 && rem<=k)
+        {
+            c++;
+        }
+        n=n/10;
+    }
+    if(c==s)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
