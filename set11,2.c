7a7d20adbf399900f804c5756a66d9aa2117f49e
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-03-18 21:37:56 +0530

    Create set11,2.c

---
+++ set11,2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num,a[100],s=0,i;
+scanf("%d",&num);
+for(i=0;i<num;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<num;i++)
+{
+s=s+a[i];
+}
+printf("%d",s);
+return 0;
+}
